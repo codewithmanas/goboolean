79db34485d5e006700e3f94501774e244f4517a9
Manas Ranjan Adha <manas865873@gmail.com>
2024-11-03 23:34:49 +0530

    integrate MS clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/tailwind.css";
 import "../styles/globals.css";
+// import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "GoBoolean",
@@ -24,6 +25,17 @@ export default function RootLayout({
         {/* Preconnect to Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        {/* MS Clarity Integration */}
+      {/* <Script strategy="beforeInteractive" id="ms-clarity">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "osw4s6wgxo");
+        `}
+      </Script> */}
       </head>
       <body>
         {children}
