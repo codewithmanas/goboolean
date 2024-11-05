0c41fa3818880c37854f8fa570b49ff52628bd1e
Manas Ranjan Adha <manas865873@gmail.com>
2024-11-05 18:58:41 +0530

    integrate google tag manager script and noscript

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
+        {/* <!-- Google Tag Manager --> */}
+        <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T33D74XD');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+
         {/* Google tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
@@ -55,6 +67,15 @@ export default function RootLayout({
       </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T33D74XD"
+            height="0" width="0" 
+            style={{display:"none",visibility:"hidden"}}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
