ae48b2be4aec1011131fd905db97b4a36e3fa3c5
Manas Ranjan Adha <manas865873@gmail.com>
2024-11-27 18:58:19 +0530

    fix: analytics id and metadataBase url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import "../styles/tailwind.css";
 import "../styles/globals.css";
-// import Script from "next/script";
+import Script from "next/script";
 // import { Toaster } from "@/components/ui/toaster";
 import { Toaster } from 'react-hot-toast';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://goboolean.in'),
   title:
     "GoBoolean - A Platform To Learn Web Development, Programming, AI and More",
   description:
@@ -85,54 +86,54 @@ export default function RootLayout({
         />
 
         {/* <!-- Google Tag Manager --> */}
-        {/* <Script id="google-tag-manager">
+        <Script id="google-tag-manager">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T33D74XD');
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
           `}
-        </Script> */}
+        </Script>
         {/* <!-- End Google Tag Manager --> */}
 
         {/* Google tag (gtag.js) */}
-        {/* <Script
+        <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-VG2TJ6N395"
-        /> */}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
 
         {/* Google Analytics Integration */}
-        {/* <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', 'G-VG2TJ6N395');
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
           `}
-        </Script> */}
+        </Script>
 
         {/* MS Clarity Integration */}
-        {/* <Script strategy="beforeInteractive" id="ms-clarity">
+        <Script strategy="afterInteractive" id="ms-clarity">
           {`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "osw4s6wgxo");
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
           `}
-        </Script> */}
+        </Script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
-        {/* <noscript>
+        <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T33D74XD"
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
             height="0" width="0" 
             style={{display:"none",visibility:"hidden"}}
           ></iframe>
-        </noscript> */}
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         <div className="min-h-screen bg-blue-950 text-white">
           <Header />
